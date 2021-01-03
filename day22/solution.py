582611f5246442e3ad22294cc23b006a8ebdbd66
thots <thots@cphnano.com>
2021-01-03 22:52:57 +0100

    Day 22, part 2

--- day22/solution.py
+++ day22/solution.py
@@ -27,9 +27,45 @@ def play(deck1, deck2):
         deck2.append(card1)
 
 
-def play_until_win(deck1, deck2):
+def crab_combat(deck1, deck2):
     while deck1 and deck2:
         play(deck1, deck2)
+
+
+def serialize_configuration(deck1, deck2):
+    return ','.join(str(card) for card in deck1) + ';' + ','.join(str(card) for card in deck2)
+
+
+def recursive_crab_combat(deck1, deck2):
+    # make a local copy so we can modify the deck without changing the original
+    deck1 = deque(deck1)
+    deck2 = deque(deck2)
+    played_configurations = set()  # keep track of played configurations
+    while deck1 and deck2:
+        serialized = serialize_configuration(deck1, deck2)
+        if serialized in played_configurations:
+            return True, deck1, deck2
+        played_configurations.add(serialized)
+        card1 = deck1.popleft()
+        card2 = deck2.popleft()
+        if len(deck1) >= card1 and len(deck2) >= card2:
+            if recursive_crab_combat(tuple(deck1)[:card1], tuple(deck2)[:card2])[0]:
+                deck1.append(card1)
+                deck1.append(card2)
+            else:
+                deck2.append(card2)
+                deck2.append(card1)
+        elif card1 > card2:
+            deck1.append(card1)
+            deck1.append(card2)
+        else:
+            deck2.append(card2)
+            deck2.append(card1)
+
+    if not deck2:
+        return True, deck1, deck2
+    if not deck1:
+        return False, deck1, deck2
 
 
 def winscore(deck):
@@ -43,9 +79,18 @@ if __name__ == '__main__':
     with open('input.txt') as f:
         lines = f.readlines()
 
+    # part 1
     player1, player2 = load_decks(lines)
-    play_until_win(player1, player2)
+    crab_combat(player1, player2)
     if player1:
+        print(winscore(player1))
+    else:
+        print(winscore(player2))
+
+    # part 2
+    player1, player2 = load_decks(lines)
+    result, player1, player2 = recursive_crab_combat(player1, player2)
+    if result:
         print(winscore(player1))
     else:
         print(winscore(player2))
--- day22/test.py
+++ day22/test.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 from collections import deque
-from day22.solution import load_decks, play, play_until_win, winscore
+from day22.solution import load_decks, play, crab_combat, winscore, serialize_configuration, recursive_crab_combat
 
 with open('day22/test_input.txt') as f:
     test_lines = [line for line in f.readlines()]
@@ -26,7 +26,7 @@ class TestSolution(TestCase):
     def test_play_until_win(self):
         player1 = deque([9, 2, 6, 3, 1])
         player2 = deque([5, 8, 4, 7, 10])
-        play_until_win(player1, player2)
+        crab_combat(player1, player2)
         player1_expected = deque()
         player2_expected = deque([3, 2, 10, 6, 8, 5, 9, 4, 7, 1])
         self.assertEqual(player1_expected, player1)
@@ -35,3 +35,34 @@ class TestSolution(TestCase):
     def test_winscore(self):
         player2 = deque([3, 2, 10, 6, 8, 5, 9, 4, 7, 1])
         self.assertEqual(306, winscore(player2))
+
+    def test_serialize_configuration(self):
+        player1 = deque([9, 2, 6, 3, 1])
+        player2 = deque([5, 8, 4, 7, 10])
+        expected = '9,2,6,3,1;5,8,4,7,10'
+        self.assertEqual(expected, serialize_configuration(player1, player2))
+
+    def test_recursive_crab_combat_empty_deck(self):
+        player1 = deque([9, 2, 6, 3, 1])
+        player2 = deque()
+        res, player1_res, player2_res = recursive_crab_combat(player1, player2)
+        self.assertTrue(res)
+        self.assertTupleEqual(tuple(player1), tuple(player1_res))
+        self.assertTupleEqual(tuple(player2), tuple(player2_res))
+        res, player2_res, player1_res = recursive_crab_combat(player2, player1)
+        self.assertFalse(res)
+        self.assertTupleEqual(tuple(player1), tuple(player1_res))
+        self.assertTupleEqual(tuple(player2), tuple(player2_res))
+
+    def test_recursive_crab_combat(self):
+        player1 = deque([9, 2, 6, 3, 1])
+        player2 = deque([5, 8, 4, 7, 10])
+        player1_wins, player1_res, player2_res = recursive_crab_combat(player1, player2)
+        self.assertFalse(player1_wins)
+        self.assertListEqual([], list(player1_res))
+        self.assertListEqual([7, 5, 6, 2, 4, 1, 10, 8, 9, 3], list(player2_res))
+
+    def test_recursive_crab_combat2(self):
+        player1 = deque([43, 19])
+        player2 = deque([2, 29, 14])
+        player1_wins, player1_res, player2_res = recursive_crab_combat(player1, player2)
